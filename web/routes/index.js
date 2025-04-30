45e60b065cfedbf950c6110d19863f312dac888f
JJ <166308221+no1jj@users.noreply.github.com>
2025-04-30 20:30:16 +0900

    index

--- web/routes/index.js
+++ web/routes/index.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.redirect('https://github.com/no1jj/restore-bot');
-});
+    res.render('index', {
+      title: '환영합니다',
+      message: '우리 프로젝트에 방문해 주셔서 감사합니다.',
+      discordUrl: 'https://discord.gg/tVCyWkTG5m',
+      githubUrl: 'https://github.com/no1jj/restore-bot'
+    });
+  });
 
 router.get('/privacypolicy', (req, res) => {
     res.render('privacy_policy');
